33c09c227ff521ce330f27591dc285b4534c0015
Kruti Modekurty <kushikruti@gmail.com>
2020-07-18 23:40:29 -0700

    Adding comments

--- utdcovid.py
+++ utdcovid.py
@@ -3,12 +3,19 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.utdallas.edu/coronavirus/confirmed-cases/'
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='cases')
 
+#find the div in the html
+results = soup.find(id='cases')
 covid_elems = results.find_all('section', class_='padding white-back mb-4')
 
+#list comprehension
 for covid_elem in covid_elems:
 	text = covid_elem.find('li', class_='padding white-back mb-4')
 	print(text)
+
+# To Do
+# Use Twilio for texts
+# Run Everyday
+# Check to see new updates
+# AWS Lambda
