0eba14ad7994fc4e7bc9df23ba176d32cae02ffe
Kruti Modekurty <kushikruti@gmail.com>
2020-07-19 00:10:32 -0700

    Correct Code

--- utdcovid.py
+++ utdcovid.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from twilio.rest import Client
 
 URL = 'https://www.utdallas.edu/coronavirus/confirmed-cases/'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#find the div in the html
-results = soup.find(id='cases')
-covid_elems = results.find_all('section', class_='padding white-back mb-4')
+#find the directory in the html
+covid_elems = soup.find_all('div', class_='padding white-back mb-4')
+listings = covid_elems[1].find_all('li')
 
 #list comprehension
-for covid_elem in covid_elems:
-	text = covid_elem.find('li', class_='padding white-back mb-4')
-	print(text)
+listings_text = [listing.get_text() for listing in listings]
+print(listings_text)
 
 # To Do
 # Use Twilio for texts
