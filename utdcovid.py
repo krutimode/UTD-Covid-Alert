1a5884b4034fc8165d3401d194af795d9f7e2283
Kruti Modekurty <kushikruti@gmail.com>
2020-07-19 00:45:18 -0700

    add twilio functionality

--- utdcovid.py
+++ utdcovid.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
@@ -14,8 +15,18 @@ listings = covid_elems[1].find_all('li')
 listings_text = [listing.get_text() for listing in listings]
 print(listings_text)
 
+first = listings_text[0]
+
+#twilio things
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+fromnum = os.environ['TWILIO_NUMBER']
+tonum = os.environ['TO_NUMBER']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(body =first, from_ = fromnum,to = tonum)
+
 # To Do
-# Use Twilio for texts
 # Run Everyday
 # Check to see new updates
 # AWS Lambda
